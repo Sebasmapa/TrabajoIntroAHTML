f6b19761f4c9e208712fca8a7986cb8bbe223ef6
Sebasmapa <127358258+Sebasmapa@users.noreply.github.com>
2024-08-26 12:06:41 -0500

    Creación inicial del dashboard

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-   
+document.addEventListener('DOMContentLoaded', function () {
+
     // Formulario de Login
     var loginForm = document.getElementById('loginForm');
-    if (loginForm){
-        loginForm.addEventListener('submit', function(event) {
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (event) {
             event.preventDefault();
             var username = document.getElementById('username').value;
             var password = document.getElementById('password').value;
-            
+
             if (username === 'admin' && password === '1234') {
                 window.location.href = 'dashboard.html';
             } else {
@@ -15,17 +15,35 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
-        // Formulario de Registro
+    // Formulario de Registro
     var registroForm = document.getElementById('registroForm');
     if (registroForm) {
-        registroForm.addEventListener('submit', function(event) {
+        registroForm.addEventListener('submit', function (event) {
             event.preventDefault();
             var newUser = document.getElementById('newUser').value;
             var newPassword = document.getElementById('newPassword').value;
-            
+
             alert('Usuario registrado: ' + newUser);
             window.location.href = 'index.html';
         });
     }
-    });
+
+    // Navegación del Dashboard
+    var linkEstudiantes = document.getElementById('linkEstudiantes');
+    var linkAsignaturas = document.getElementById('linkAsignaturas');
+    var contenedorEstudiantes = document.getElementById('contenedorEstudiantes');
+    var contenedorAsignaturas = document.getElementById('contenedorAsignaturas');
+
+        linkEstudiantes.addEventListener('click', function (event) {
+            event.preventDefault();
+            contenedorEstudiantes.classList.add('active');
+            contenedorAsignaturas.classList.remove('active');
+        });
+
+        linkAsignaturas.addEventListener('click', function (event) {
+            event.preventDefault();
+            contenedorAsignaturas.classList.add('active');
+            contenedorEstudiantes.classList.remove('active');
+        });
+});
 
